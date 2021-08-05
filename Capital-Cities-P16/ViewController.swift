c2b44b4ea1984d52e167972a605ea5f3311fc4d7
Łukasz Nycz <lnycz96@gmail.com>
2021-08-05 12:29:19 +0200

    Initial Commit

---
+++ Capital-Cities-P16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capital-Cities-P16
+//
+//  Created by Łukasz Nycz on 05/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
