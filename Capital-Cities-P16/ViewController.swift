662530199279386fc10cb5535b1977d3fc4598af
Łukasz Nycz <lnycz96@gmail.com>
2021-08-08 21:17:31 +0200

    Challenge
    Challenge
    One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try your new knowledge to make sure you fully understand what’s going on:
    
    Try typecasting the return value from dequeueReusableAnnotationView() so that it's an MKPinAnnotationView. Once that’s done, change the pinTintColor property to your favorite UIColor.
    Add a UIAlertController that lets users specify how they want to view the map. There's a mapType property that draws the maps in different ways. For example, .satellite gives a satellite view of the terrain.
    Modify the callout button so that pressing it shows a new view controller with a web view, taking users to the Wikipedia entry for that city.

--- Capital-Cities-P16/ViewController.swift
+++ Capital-Cities-P16/ViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import MapKit
+import SafariServices
 
 class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.50722, longitude: -0.1275), info: "Home to 2012 Summer Olimpics.")
         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "It was founded over 1000 years ago.")
@@ -21,15 +23,47 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 48.895111, longitude: -77.036667), info: "Named after George himself.")
             
         mapView.addAnnotations([london, oslo, paris, rome, washington])
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(mapViewType))
        
 
+    }
+    @objc func mapViewType() {
+        let ac = UIAlertController(title: "Choose map view type", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Hybrid", style: .default, handler: viewType))
+        ac.addAction(UIAlertAction(title: "HybridFlyover", style: .default, handler: viewType))
+        ac.addAction(UIAlertAction(title: "MutedStandard", style: .default, handler: viewType))
+        ac.addAction(UIAlertAction(title: "Satellite", style: .default, handler: viewType))
+        ac.addAction(UIAlertAction(title: "SatelliteFlyover", style: .default, handler: viewType))
+        ac.addAction(UIAlertAction(title: "Standard", style: .default, handler: viewType))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(ac, animated: true)
+
+    }
+    func viewType(action: UIAlertAction) {
+        switch action.title {
+        case "Hybrid":
+            mapView.mapType = .hybrid
+        case "HybridFlyover":
+            mapView.mapType = .hybridFlyover
+        case "MutedStandard":
+            mapView.mapType = .mutedStandard
+        case "Satellite":
+            mapView.mapType = .satellite
+        case "SatelliteFlyover":
+            mapView.mapType = .satelliteFlyover
+        case "Standard":
+            mapView.mapType = .standard
+        default:
+            mapView.mapType = .standard
+        }
     }
 
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
             guard annotation is Capital else {return nil}
              let identifier = "Capital"
             
-            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
             
             if annotationView == nil {
                 annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
@@ -41,16 +75,15 @@ class ViewController: UIViewController, MKMapViewDelegate {
             } else {
                 annotationView?.annotation = annotation
         }
+            annotationView?.pinTintColor = .systemBlue
          return annotationView
     }
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         guard let capital = view.annotation as? Capital else {return}
+        let capitalTitle = capital.title!
         
-        let placeName = capital.title
-        let placeInfo = capital.info
-        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true, completion: nil)
+        let vc = SFSafariViewController(url: URL(string: "https://en.wikipedia.org/wiki/" + "\(capitalTitle)")!)
+        present(vc, animated: true)
     }
 
 }
